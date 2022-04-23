5e8ed7c2a4c45e44afa1d20e0ba310d62f93d63d
Jaswal1p <Jaswal1p@yahoo.com>
2022-04-23 04:35:25 -0400

    added code for 'me'query & mutation for login in resolvers.js in schemas

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,45 @@
+const { User, Book } = require('../models');
+const { signToken } = require('../utils/auth');
+const { AuthenticationError } = require('apollo-server-express');
+
+
 const resolvers = {
-    Query: {
-      helloWorld: () => {
-        return 'Hello world!';
-      }
+  Query: {
+    me: async (parent, args, context) => {
+        if(context.user) {
+            const userData = await User.findOne({ _id: context.user._id })
+                .select('-__v -password')
+
+            return userData;
+        }
+
+        throw new AuthenticationError('Not logged in');
     }
-  };
+  },
+
+  Mutation: {
+
+    login: async (parent, { email, password }) => {
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            throw new AuthenticationError('Incorrect credentials');
+        }
+
+        const correctPw = await user.isCorrectPassword(password);
+
+        if (!correctPw) {
+            throw new AuthenticationError('Incorrect credentials');
+        }
+
+        const token = signToken(user);
+        return { token, user };
+      },
+  },
+
+
+
+
+};
   
   module.exports = resolvers;
