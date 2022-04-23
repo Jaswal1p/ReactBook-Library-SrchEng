aea368c4ded074537166e2e9a8fbdb248027e5af
Jaswal1p <Jaswal1p@yahoo.com>
2022-04-23 17:22:28 -0400

    added code to searchBooks.js to import utils/mutations & apollo reacthooks

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -1,9 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
 
+import React, {useState, useEffect} from 'react';
+
 import Auth from '../utils/auth';
+
+import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
+
 import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
+
+import {SAVE_BOOK} from '../utils/mutations';
+
+import {useMutation} from '@apollo/react-hooks';
+
+
 
 const SearchBooks = () => {
   // create state for holding returned google api data
@@ -65,9 +76,11 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
+      const {data} = await saveBook({
+         variables: {input: bookToSave}
+      });
 
-      if (!response.ok) {
+      if (error) {
         throw new Error('something went wrong!');
       }
 
